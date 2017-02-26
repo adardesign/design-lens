6166b8ea77aabc59cb50e228a04dfa64f36b3d1d
Eliazer Braun <adardesign@gmail.com>
2017-02-26 16:14:35 -0500

    node express template progress

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -1,9 +1,29 @@
 $(function(){
 	$(".commandments-list").on("click", ".arrow", function toggle() {
-		$(this).closest("li").find(".preview").slideToggle();
+		var jThis = $(this),
+			li = jThis.closest("li"),
+			shownClass = "preview-shown";
+		
+		li.toggleClass(shownClass).siblings().removeClass(shownClass).find(".commandment-preview-container").slideUp();
+		li.find(".commandment-preview-container").slideToggle();
 	});
 
 	$(".hamburger-container").on("click", function onHaburgerClick() {
 		$("body").toggleClass("nav-open");
 	});
+
+
+	postComment = function postComment() {
+		fetch('quotes', {
+  			method: 'put',
+  			headers: {'Content-Type': 'application/json'},
+  			body: JSON.stringify({
+    		'name': 'Darth Vader',
+    		'quote': 'I find your lack of faith disturbing.'
+  		})
+	})
+
+	}
 });
+
+
--- server/app.js
+++ server/app.js
@@ -1,9 +1,46 @@
 var express = require('express');
+var bodyParser= require('body-parser')
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/../client/'));
+app.use(bodyParser.urlencoded({extended: true}))
+app.set('view engine', 'ejs')
 
-app.listen(app.get('port'), function() {
-	console.log('Node app is running on port', app.get('port'));
+app.set('port', (process.env.PORT || 5000));
+//app.use(express.static(__dirname + '/../client/'));
+app.use('/client', express.static(__dirname + '/../client/'));
+
+//app.listen(app.get('port'), function() {
+//	console.log('Node app is running on port', app.get('port'));
+//});
+
+
+var MongoClient = require('mongodb').MongoClient;
+var	db;
+
+
+MongoClient.connect("mongodb://adardesign:designlensmoshe@ds161099.mlab.com:61099/design-lens", (err, database) => {
+    if (err) return console.log(err)
+  	db = database
+  	app.listen(app.get('port'), () => {
+    	console.log('listening')
+  })
 });
+
+
+app.get('/', (req, res) => {
+  db.collection('commandments').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    // renders index.ejs
+	console.log(result[0])
+	res.render('../views/index.ejs', {commandments:result[0].commandments})
+  });
+});
+
+app.post('/comment', (req, res) => {
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
